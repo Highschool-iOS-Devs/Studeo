e780737036bfdebc500755e966a3e1873593e27f
AndreasInk <andreasink17@gmail.com>
2020-08-22 18:11:25 -0400

    Organized files, added UserData

---
+++ StudyHub/Data/UserData.swift
@@ -0,0 +1,21 @@
+//
+//  UserData.swift
+//  StudyHub
+//
+//  Created by Andreas Ink on 8/22/20.
+//  Copyright © 2020 Dakshin Devanand. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserData_Previews: PreviewProvider {
+    static var previews: some View {
+        UserData()
+    }
+}
