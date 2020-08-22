f0ea5c0321b4d946e36cf833de6e08228ae41ea8
AndreasInk <andreasink17@gmail.com>
2020-08-22 19:42:52 -0400

    Implemented searchbar on the home view

--- StudyHub/Views/Home/Home.swift
+++ StudyHub/Views/Home/Home.swift
@@ -9,10 +9,30 @@
 import SwiftUI
 
 struct Home: View {
+    
     @EnvironmentObject var userData: UserData
+    @State private var search: String = ""
     var body: some View {
         VStack {
             Header()
+            ZStack {
+                HStack {
+                    Spacer()
+                    Divider()
+                        .frame(height: 25)
+                    Image("dropdown")
+                        .renderingMode(.original)
+                        .resizable()
+                        .frame(width: 25, height: 25)
+                        .aspectRatio(contentMode: .fit)
+                } .padding(.horizontal, 44)
+                TextField("Search", text: $search)
+                    .font(Font.custom("Montserrat-Regular", size: 15.0))
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(10.0)
+                  
+            } .padding(.horizontal, 22)
             Spacer()
         }
     }
