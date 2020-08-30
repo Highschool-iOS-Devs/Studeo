6a2efd5004b0b982fb7413dc1c5cc50e3e5c9080
AndreasInk <andreasink17@gmail.com>
2020-08-30 13:17:34 -0400

    Cleaning up

--- StudyHub/ChatList.swift
+++ StudyHub/ChatList.swift
@@ -15,14 +15,9 @@ struct ChatList: View {
     @State var tapped = false
     @State var add = false
     @State var auth = false
-    @State var index = -1
-    @State var i = 0
-    @State var total = -1
     @State var personCount = -1
-    @State var interactedCount = -1
     @State var chatRoom = [""]
     @State var userIDs = [""]
-     @State var num = -1
     @State private var activeSheet: ActiveSheet = .first
     enum ActiveSheet {
         case first, second
@@ -31,13 +26,13 @@ struct ChatList: View {
         ZStack {
             Color(.white)
                 .onAppear() {
-                    self.num = 0
+                    
                     self.personCount = -1
-                    self.interactedCount = -1
+                    
                     self.hasData = false
                     self.people.removeAll()
                     self.chatRoom.removeAll()
-                   print(Auth.auth().currentUser?.uid)
+                    print(Auth.auth().currentUser?.uid)
                     if Auth.auth().currentUser?.uid == nil {
                         self.auth = true
                     } else {
@@ -45,14 +40,14 @@ struct ChatList: View {
                         self.loadData()
                     }
             }
-        
-                 
-        
-                
-                        if self.auth {
-                            Color(.white)
-                            RegistrationView()
-                        }
+            
+            
+            
+            
+            if self.auth {
+                Color(.white)
+                RegistrationView()
+            }
             if Auth.auth().currentUser?.uid != nil {
                 if hasData {
                     Color(.white)
@@ -69,7 +64,7 @@ struct ChatList: View {
                                             self.activeSheet = .second
                                             self.tapped.toggle()
                                             
-                                        }
+                                    }
                                     Spacer()
                                 }
                                 
@@ -127,7 +122,7 @@ struct ChatList: View {
         db.collection("users").document(Auth.auth().currentUser!.uid).addSnapshotListener { (document, error) in
             if let document = document, document.exists {
                 
-                self.index += 1
+                
                 let name = document.get("name") as! String
                 let id = document.get("id") as! String
                 let hasInteractedWith = document.get("hasInteractedWith") as! [String]
@@ -136,54 +131,47 @@ struct ChatList: View {
                 self.userIDs = hasInteractedWith
                 for interacted in hasInteractedWith {
                     
-               db.collection("users").document(interacted).getDocument { (document, error) in
-                if let document = document, document.exists {
-                  
-                    
-                    self.interactedCount += 1
-                    self.index = self.index + 1
-                    let name = document.get("name") as! String
-                    let id = document.get("id") as! String
-                    print(id)
-                    self.num += 1
-                    
-                     
-                    
-                   
-                    print(self.chatRoom)
-                    if !self.tapped {
-                        self.personCount += 1
-                    self.people.append(ChattedWith(id: id, name: name, count: self.personCount, chatRoom: self.chatRoom[self.personCount]))
-                        
-                      
-                               print("chatroom ids =   \(self.chatRoom)")
-                        print("user ids =   \(self.userIDs)")
-                        
-                    }
-                        // for person in self.people {
-                        
-                      //  if person.id == Auth.auth().currentUser!.uid {
+                    db.collection("users").document(interacted).getDocument { (document, error) in
+                        if let document = document, document.exists {
                             
-                          //  print("Remove")
-                           // self.people.remove(at: self.num)
-                          // self.num -= 1
-                         //  self.personCount -= 1
-                      // }
-                    //    print("Add")
-                       
-                          self.hasData = true
+                            let name = document.get("name") as! String
+                            let id = document.get("id") as! String
+                            print(id)
+                            
+                            print(self.chatRoom)
+                            if !self.tapped {
+                                self.personCount += 1
+                                self.people.append(ChattedWith(id: id, name: name, count: self.personCount, chatRoom: self.chatRoom[self.personCount]))
+                                
+                                
+                                print("chatroom ids =   \(self.chatRoom)")
+                                print("user ids =   \(self.userIDs)")
+                                
+                            }
+                            // for person in self.people {
+                            
+                            //  if person.id == Auth.auth().currentUser!.uid {
+                            
+                            //  print("Remove")
+                            // self.people.remove(at: self.num)
+                            // self.num -= 1
+                            //  self.personCount -= 1
+                            // }
+                            //    print("Add")
+                            
+                            self.hasData = true
                         }
-                   
+                        
                     }
                     print(self.personCount)
-                 //   print(self.people[self.personCount].chatRoom)
-                   
+                    //   print(self.people[self.personCount].chatRoom)
+                    
                 }
-                            }
-                        }
-             //   print(self.index)
+            }
+        }
+        //   print(self.index)
+    }
 }
-}
-                
-    
-    
+
+
+
