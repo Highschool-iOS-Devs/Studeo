85ace5df304b0ad7d2632c63f42063577bc09eff
AndreasInk <andreasink17@gmail.com>
2020-09-17 19:58:42 -0400

    Readded SettingsData

---
+++ SettingsData.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsData.swift
+//  
+//
+//  Created by Andreas Ink on 9/17/20.
+//
+
+import SwiftUI
+
+struct SettingsData: Identifiable, Codable {
+    var id: String
+    var name: String
+    var state: String
+   
+
+}
