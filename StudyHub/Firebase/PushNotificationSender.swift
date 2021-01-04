acfd1b207c2b497fb28da9a7c7b9051598cf671f
Andreas Ink <andreasink17@gmail.com>
2021-01-03 19:38:30 -0500

    Push Noti implementation

--- StudyHub/Firebase/PushNotificationSender.swift
+++ StudyHub/Firebase/PushNotificationSender.swift
@@ -22,7 +22,7 @@ class PushNotificationSender {
         request.httpMethod = "POST"
         request.httpBody = try? JSONSerialization.data(withJSONObject:paramString, options: [.prettyPrinted])
         request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.setValue("key=AAAArGyKLow:APA91bEML0-9unoUNCWLiyx6IOBgPzRDc5J-KZ_bdhH3CfQyGuLoT3SI5KqNwI2WxebW1MFbCjGxyjs3tsqiBzB9WG908gtvpfUBS_eKyZX65I42GCDCUPfz_V36w5dzLG1gYHkalF3X", forHTTPHeaderField: "Authorization")
+        request.setValue("key=AAAAmmjN4M0:APA91bEq8j2yeJVLZWxqQ31tzX6M_MH9IFl5WGi72t9QEQiY5-zR0M7N2ADky-3m1i7tj1KwDHBUiPsJDU440GbnW1Ya2vcBhSdfrcJuwDiy1sqt5cDiQDWEz3j4OuOLyAbjCUbIDSXG", forHTTPHeaderField: "Authorization")
 
         let task =  URLSession.shared.dataTask(with: request as URLRequest)  { (data, response, error) in
             do {
