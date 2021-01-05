a2e9488ba8933d84ee0b213db1de1d5163da1176
Andreas Ink <andreasink17@gmail.com>
2021-01-04 20:12:25 -0500

    Settings UI bug fixes

--- StudyHub/Views/Home/CTA.swift
+++ StudyHub/Views/Home/CTA.swift
@@ -40,7 +40,7 @@ struct CTA: View {
                         .padding(.leading, 30)
                     Button(action: {
                         self.userData.tappedCTA = true
-                        if self.cta == "Add Friends" {
+                        if self.cta == "Find Study Partners" {
                             self.tabRouter.currentView = .chatList
                             
                             
