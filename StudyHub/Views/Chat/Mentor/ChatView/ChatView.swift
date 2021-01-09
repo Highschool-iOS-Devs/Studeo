ec9dd7a02a7a3fb6531e112e0b65f8eaa992d998
Andreas Ink <andreasink17@gmail.com>
2021-01-09 10:56:24 -0500

    Fixed chat view ui issue

--- StudyHub/Views/Chat/Mentor/ChatView/ChatView.swift
+++ StudyHub/Views/Chat/Mentor/ChatView/ChatView.swift
@@ -85,8 +85,7 @@ struct ChatView: View {
                                         if message.sentBySelf ?? false {
                                             Spacer()
                                         }
-                                    Text(message.sentByName)
-                                        .font(.custom("Montserrat Light", size: 10))
+                                    
                                         
                                         if !message.sentBySelf! {
                                             Spacer()
@@ -366,7 +365,7 @@ struct ChatView: View {
             }
      
             
-            
+      
     
             
             
